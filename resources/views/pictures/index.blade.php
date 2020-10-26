4667b2ff56713b0b07eaa73b8202b07323652e40
shonansurvivors <shonansurvivors@gmail.com>
2020-10-27 00:36:34 +0900

    スマホは2列に

--- resources/views/pictures/index.blade.php
+++ resources/views/pictures/index.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
   <div class="container-fluid">
-    <div class="row justify-content-center">
+    <div class="row">
 
       <div class="card-deck">
         @foreach($pictures as $picture)
-          <div class="col-md-2">
+          <div class="col-sm-6 col-md-2">
             <div class="card">
 
               <div class="card-body">
