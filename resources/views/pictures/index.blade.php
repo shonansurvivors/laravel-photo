ddcada3138d56b7bbdb1d9a2c6b0736f9e8643f3
shonansurvivors <shonansurvivors@gmail.com>
2020-10-27 00:16:49 +0900

    6枚で折り返すように

--- resources/views/pictures/index.blade.php
+++ resources/views/pictures/index.blade.php
@@ -4,19 +4,19 @@
   <div class="container">
     <div class="row justify-content-center">
 
-      <div class="col-md-12">
-        <div class="card-deck">
-          @foreach($pictures as $picture)
-          <div class="card">
+      <div class="card-deck">
+        @foreach($pictures as $picture)
+          <div class="col-md-2">
+            <div class="card">
 
-            <div class="card-body">
+              <div class="card-body">
 
-              <img src="{{ asset('storage/' . $picture->filepath) }}" class="card-img">
+                <img src="{{ asset('storage/' . $picture->filepath) }}" class="card-img">
 
+              </div>
             </div>
           </div>
-          @endforeach
-        </div>
+        @endforeach
       </div>
 
     </div>
