836847b29e4a76167892d7afc21687035f9c39fd
shonansurvivors <shonansurvivors@gmail.com>
2020-10-27 09:25:07 +0900

    投稿ボタン

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -26,6 +26,10 @@
             </li>
           @endif
         @else
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('pictures.create') }}"><i class="fas fa-pen mr-1"></i>投稿する</a>
+          </li>
+
           <li class="nav-item dropdown">
             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
               {{ Auth::user()->name }} <span class="caret"></span>
